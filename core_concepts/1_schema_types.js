718ed4bc69ed44ba18bfc2e1b68f99e7f1d3fe04
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2020-01-10 10:54:25 +0100

    schema types

---
+++ core_concepts/1_schema_types.js
@@ -0,0 +1,12 @@
+// GraphQL uses the Schema Definition Language (DSL)
+
+// DEFINING SIMPLE TYPES - Exclamation sign (!) signifies required fields
+
+type Person {
+  name: String!
+  age: Int!
+}
+
+type Post {
+  title: String!
+}
