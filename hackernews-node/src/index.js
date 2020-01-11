2eacf67891fe72f18f70c9df0c5fb23f36daf341
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2020-01-12 00:41:52 +0100

    vote feature with subscription

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -6,6 +6,7 @@ const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
 const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
 // the actual implementation of the GraphQL schema
 const resolvers = {
@@ -13,7 +14,8 @@ const resolvers = {
   Mutation,
   Subscription,
   User,
-  Link
+  Link,
+  Vote,
 }
 
 const server = new GraphQLServer({
