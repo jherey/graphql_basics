e3e4fc137492cfdeff9a40364fb9ad89c8553daa
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2020-01-11 15:58:42 +0100

    add mutations

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,19 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-// defines your GraphQL schema
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
-
 let links = [
   {
     id: 'link-0',
@@ -23,20 +9,54 @@ let links = [
 ];
 
 // the actual implementation of the GraphQL schema
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () =>  `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: () => links,
+    link: (parent, args) => {
+      const { id } = args;
+      const link = links.find(link => link.id === id);
+      return link ? link : null;
+    }
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const { id, description, url } = args;
+      let link = links.find(link => link.id === id);
+      if (!link) return null;
+      link.url = url || link.url;
+      link.description = description || link.description;
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      const { id } = args;
+      const linkToDeleteIndex = links.findIndex(link => link.id === id);
+      const link = links[linkToDeleteIndex];
+      if (linkToDeleteIndex === -1) return null;
+      links.splice(linkToDeleteIndex, 1);
+      return link;
+    }
+  },
+  // Removing the Link resolves because the GraphQL server infers what they look like
+  // Link: {
+  //   id: (parent) => parent.id,
+  //   description: (parent) => parent.description,
+  //   url: (parent) => parent.url,
+  // }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 
